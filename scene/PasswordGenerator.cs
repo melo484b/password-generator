297478850dc99a5f23bafd585d6e6828d67d2e63
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-03 23:23:53 -0400

    Upload password generator

---
+++ scene/PasswordGenerator.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public class PasswordGenerator : MarginContainer
+{
+	private const int MAX_LENGTH = 20;
+	private const string LOWER_CASE_LETTERS = "abcdefghijklmnopqrstuvwxyz"; 
+	private const string UPPER_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private const string NUMBERS = "1234567890";
+	private const string SYMBOLS = "~`!@#$%^&*()_-+={[}]|:;'<,>.?/";
+	
+	private string[] characterStrings = {LOWER_CASE_LETTERS, UPPER_CASE_LETTERS, NUMBERS, SYMBOLS};
+	
+	TextEdit textBox;
+	
+	public override void _Ready()
+	{
+		textBox = (TextEdit) GetNode("PanelContainer/MarginContainer/VBoxContainer/TextBox");
+		GeneratePassword();
+	}
+
+	private void OnGenerateButtonPressed()
+	{
+		GeneratePassword();
+	}
+	
+	private void GeneratePassword()
+	{
+		Random random = new Random();
+		string password = "";
+		for (int i = 0; i <= MAX_LENGTH; i++)
+		{
+			int stringIndex = random.Next(characterStrings.Length);
+			int characterIndex = random.Next(characterStrings[stringIndex].Length);
+			password += characterStrings[stringIndex][characterIndex];
+		}
+		textBox.Text = password;
+	}
+	
+}
